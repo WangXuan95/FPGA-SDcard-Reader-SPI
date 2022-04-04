675d8ed386606da432c2f0bfb007882e949b11c9
WangXuan95 <629708558@qq.com>
2022-04-04 21:18:44 +0800

    update

--- RTL/sd_spi_sector_reader.sv
+++ RTL/sd_spi_sector_reader.sv
@@ -34,70 +34,71 @@ localparam CMD8_VALID_RES  = 8'hAA;
 enum logic [1:0] {NONE, SDv1, SDv2, SDHCv2} cardtype = NONE;
 enum logic [3:0] {RESET, CMD0, CMD8, CMD1, CMD8FAILED, ACMD41, CMD58, CMD16, IDLE, READING} cardstate = RESET;
 
-reg  spistart=0;
+reg  spistart=1'b0;
 wire spidone;
 reg  [47:0] cmd=0, acmd=0;
 wire [47:0] cmdres;
-reg  [ 7:0] waitcycle=0, precycle=0, startcycle=0, cmdrcycle=0, acmdcycle=0, midcycle=0, recycle=0;
+reg  [ 7:0] waitcycle=64, precycle=0, startcycle=0, cmdrcycle=0, acmdcycle=0, midcycle=0, recycle=0;
 wire [ 7:0] cmdrsp, acmdrsp, rwrsp;
 reg  [31:0] target_sector = 0;
+wire [31:0] target_sector_w = (cardtype==SDHCv2) ? sector_no : ( (cardtype==SDv2 || cardtype==SDv1) ? sector_no*512 : 0 );
 
 initial done = 1'b0;
-initial begin rvalid = 1'b0; raddr  = 9'h0; rdata  = 8'h0; end
-assign card_type  = cardtype;
+initial begin rvalid = 1'b0; raddr = 9'h0; rdata = 8'h0; end
+assign card_type = cardtype;
 assign card_stat = cardstate;
 
 always @ (posedge clk or negedge rstn) begin
     if(~rstn) begin
-        done = 1'b0;
-        spistart=0; cmd=48'h00_00000000_00; acmd=48'h00_00000000_00; waitcycle= 64; precycle=  0; startcycle=0; cmdrcycle=0; acmdcycle=0; midcycle= 0;recycle=0;
-        cardtype  =  NONE;
-        cardstate = RESET;
-        target_sector = 0;
+        done <= 1'b0;
+        spistart<=1'b0; cmd<=48'h00_00000000_00; acmd<=48'h00_00000000_00; waitcycle<= 64; precycle<=  0; startcycle<=0; cmdrcycle<=0; acmdcycle<=0; midcycle<= 0;recycle<=0;
+        cardtype  <=  NONE;
+        cardstate <= RESET;
+        target_sector <= 0;
     end else begin
-        done = 1'b0;
-        spistart=0; cmd=48'h00_00000000_00; acmd=48'h00_00000000_00; waitcycle= 64; precycle=  0; startcycle=0; cmdrcycle=0; acmdcycle=0; midcycle= 0;recycle=0;
+        done <= 1'b0;
+        spistart<=1'b0; cmd<=48'h00_00000000_00; acmd<=48'h00_00000000_00; waitcycle<= 64; precycle<=  0; startcycle<=0; cmdrcycle<=0; acmdcycle<=0; midcycle<= 0;recycle<=0;
         if(spidone) begin
             case(cardstate)
-            RESET : cardstate = CMD0;
-            CMD0  : if(cmdrsp==8'h01) cardstate = CMD8;
-            CMD1  : if(cmdrsp==8'h00) cardstate = IDLE;
+            RESET : cardstate <= CMD0;
+            CMD0  : if(cmdrsp==8'h01) cardstate <= CMD8;
+            CMD1  : if(cmdrsp==8'h00) cardstate <= IDLE;
             CMD8  : if(cmdrsp==8'h01) begin  // SDv2.0
                         if(cmdres[0+:8]==CMD8_VALID_RES)  // CMD8 success
-                            cardstate = ACMD41;
+                            cardstate <= ACMD41;
                         else
-                            cardstate = CMD8FAILED;
+                            cardstate <= CMD8FAILED;
                     end else begin   // SDv1
-                        cardstate = CMD1;   // TODO: SDv1
-                        cardtype  = SDv1;
+                        cardstate <= CMD1;   // TODO: SDv1
+                        cardtype  <= SDv1;
                     end
-            ACMD41: if(cmdrsp==8'h01 && acmdrsp==8'h00) cardstate = CMD58;
+            ACMD41: if(cmdrsp==8'h01 && acmdrsp==8'h00) cardstate <= CMD58;
             CMD58 : if(~cmdrsp[7]) begin  // SDv2.0
                         if((cmdres[3*8+:8]&8'hC0)==8'hC0)  // done initialize, SDHCv2
-                            cardtype  = SDHCv2;
+                            cardtype  <= SDHCv2;
                         else                               // done initialize, SDv2
-                            cardtype  = SDv2;
-                        cardstate = CMD16;
+                            cardtype  <= SDv2;
+                        cardstate <= CMD16;
                     end
-            CMD16 :    if(cmdrsp==8'h00)  cardstate = IDLE;
-            IDLE  :    if( (~cmdrsp[7]) && rwrsp==8'hFE) begin cardstate = READING;            end
-            READING  : if( (~cmdrsp[7]) && rwrsp==8'hFE) begin cardstate = IDLE   ; done=1'b1; end
+            CMD16 :    if(cmdrsp==8'h00)  cardstate <= IDLE;
+            IDLE  :    if( (~cmdrsp[7]) && rwrsp==8'hFE) begin cardstate <= READING;            end
+            READING  : if( (~cmdrsp[7]) && rwrsp==8'hFE) begin cardstate <= IDLE   ; done<=1'b1; end
             endcase
         end else begin
             case(cardstate)
-            RESET  : cardstate = CMD0;
-            CMD0   : begin spistart=1; cmd=48'h40_00000000_95 ; acmd=48'h00_00000000_00; waitcycle=255; precycle=20; startcycle=0; cmdrcycle=0; acmdcycle=0; midcycle= 0;recycle=0; end
-            CMD1   : begin spistart=1; cmd=48'h41_00000000_ff ; acmd=48'h00_00000000_00; waitcycle= 16; precycle= 0; startcycle=0; cmdrcycle=0; acmdcycle=0; midcycle= 0;recycle=0; end
-            CMD8   : begin spistart=1; cmd=48'h48_000001aa_87 ; acmd=48'h00_00000000_00; waitcycle= 16; precycle= 0; startcycle=1; cmdrcycle=4; acmdcycle=0; midcycle= 0;recycle=0; end
-            ACMD41 : begin spistart=1; cmd=48'h77_00000000_FF ; acmd=48'h69_40000000_FF; waitcycle= 16; precycle= 0; startcycle=1; cmdrcycle=0; acmdcycle=6; midcycle= 0;recycle=0; end
-            CMD58  : begin spistart=1; cmd=48'h7a_00000000_FF ; acmd=48'h00_00000000_00; waitcycle= 16; precycle= 0; startcycle=1; cmdrcycle=4; acmdcycle=0; midcycle= 0;recycle=0; end
-            CMD16  : begin spistart=1; cmd=48'h50_00000200_FF ; acmd=48'h00_00000000_00; waitcycle= 16; precycle= 0; startcycle=1; cmdrcycle=2; acmdcycle=0; midcycle= 0;recycle=0; end
+            RESET  : cardstate <= CMD0;
+            CMD0   : begin    spistart<=1'b1; cmd<=48'h40_00000000_95 ; acmd<=48'h00_00000000_00; waitcycle<=255; precycle<=20; startcycle<=0; cmdrcycle<=0; acmdcycle<=0; midcycle<= 0;recycle<=0; end
+            CMD1   : begin    spistart<=1'b1; cmd<=48'h41_00000000_ff ; acmd<=48'h00_00000000_00; waitcycle<= 16; precycle<= 0; startcycle<=0; cmdrcycle<=0; acmdcycle<=0; midcycle<= 0;recycle<=0; end
+            CMD8   : begin    spistart<=1'b1; cmd<=48'h48_000001aa_87 ; acmd<=48'h00_00000000_00; waitcycle<= 16; precycle<= 0; startcycle<=1; cmdrcycle<=4; acmdcycle<=0; midcycle<= 0;recycle<=0; end
+            ACMD41 : begin    spistart<=1'b1; cmd<=48'h77_00000000_FF ; acmd<=48'h69_40000000_FF; waitcycle<= 16; precycle<= 0; startcycle<=1; cmdrcycle<=0; acmdcycle<=6; midcycle<= 0;recycle<=0; end
+            CMD58  : begin    spistart<=1'b1; cmd<=48'h7a_00000000_FF ; acmd<=48'h00_00000000_00; waitcycle<= 16; precycle<= 0; startcycle<=1; cmdrcycle<=4; acmdcycle<=0; midcycle<= 0;recycle<=0; end
+            CMD16  : begin    spistart<=1'b1; cmd<=48'h50_00000200_FF ; acmd<=48'h00_00000000_00; waitcycle<= 16; precycle<= 0; startcycle<=1; cmdrcycle<=2; acmdcycle<=0; midcycle<= 0;recycle<=0; end
             IDLE   : if(start) begin
-                       target_sector = (cardtype==SDHCv2) ? sector_no : ( (cardtype==SDv2 || cardtype==SDv1) ? sector_no*512 : 0 );
-                       spistart=1; cmd={8'h51,target_sector,8'hFF}; acmd=48'h00_00000000_00; waitcycle= 6; precycle= 0; startcycle=0; cmdrcycle=4; acmdcycle=0; midcycle=99;recycle=2;
+                       target_sector <= target_sector_w;
+                       spistart<=1'b1; cmd<={8'h51,target_sector_w,8'hFF}; acmd<=48'h00_00000000_00; waitcycle<=  6; precycle<= 0; startcycle<=0; cmdrcycle<=4; acmdcycle<=0; midcycle<=99;recycle<=2;
                      end
             READING: begin
-                       spistart=1; cmd={8'h51,target_sector,8'hFF}; acmd=48'h00_00000000_00; waitcycle= 6; precycle= 0; startcycle=0; cmdrcycle=4; acmdcycle=0; midcycle=99;recycle=2;
+                       spistart<=1'b1; cmd<={8'h51,target_sector  ,8'hFF}; acmd<=48'h00_00000000_00; waitcycle<=  6; precycle<= 0; startcycle<=0; cmdrcycle<=4; acmdcycle<=0; midcycle<=99;recycle<=2;
                      end
             endcase
         end
@@ -110,18 +111,18 @@ logic [ 7:0] rdata_session;
 
 always @ (posedge clk or negedge rstn) begin
     if(~rstn) begin
-        rvalid = 1'b0;
-        raddr  = 9'h0;
-        rdata  = 8'h0;
+        rvalid <= 1'b0;
+        raddr  <= 9'h0;
+        rdata  <= 8'h0;
     end else begin
         if(cardstate==READING && rvalid_session && (rindex_session>=16'd2 && rindex_session<16'd514)) begin
-            rvalid = 1'b1;
-            raddr  = 16'd513 - rindex_session;
-            rdata  = rdata_session;
+            rvalid <= 1'b1;
+            raddr  <= 9'd1 - (9)'(rindex_session);
+            rdata  <= rdata_session;
         end else begin
-            rvalid = 1'b0;
-            raddr  = 9'h0;
-            rdata  = 8'h0;
+            rvalid <= 1'b0;
+            raddr  <= 9'h0;
+            rdata  <= 8'h0;
         end
     end
 end
--- RTL/spi_session.sv
+++ RTL/spi_session.sv
@@ -35,8 +35,8 @@ logic [ 2:0] bitcnt=3'b0;
 logic highlow=1'b0;
 logic byteend, bytestart;
 logic scken=1'b0, chipselect=1'b0;
-logic [ 7:0] wbyte=8'h0, rbyte=8'h0;
-logic [47:0] cmdr=0, acmdr=0;
+logic [ 7:0] wbyte=8'hff, rbyte=8'h0;
+logic [47:0] cmdr='0, acmdr='0;
 logic [ 7:0] waitc=8'h0, prec=8'h0, startc=8'h0, cmdc=8'h0, cmdrwait=8'h0, cmdrc=8'h0, acmdc=8'h0, acmdrwait=8'h0, acmdrc=8'h0, midc=8'h0, stopc=8'h0, rec=8'h0, lastc=8'h0;
 logic [15:0] rwc = 16'h0;
 logic iscmdr=1'b0, isacmdr=1'b0, iscmdres=1'b0, isacmdres=1'b0,ismidc=1'b0, isrwc=1'b0;
@@ -44,133 +44,132 @@ logic iscmdr=1'b0, isacmdr=1'b0, iscmdres=1'b0, isacmdres=1'b0,ismidc=1'b0, isrw
 assign byteend   = (cyccnt==0) && ({bitcnt,highlow}==4'h0) ;
 assign bytestart = (cyccnt==1) && ({bitcnt,highlow}==4'h0) ;
 
+assign done = start && start_last && (lastc==0);
+
 always @ (posedge clk or negedge rstn)
     if(~rstn) begin
-        cyccnt = 0;
-        {bitcnt,highlow} = 3'h0;
-        {spi_ssn, spi_sck, spi_mosi} = 3'b111;
-        rbyte = 8'h0;
+        cyccnt <= 0;
+        {bitcnt,highlow} <= 3'h0;
+        {spi_ssn, spi_sck, spi_mosi} <= 3'b111;
+        rbyte <= 8'h0;
     end else begin
         if(~start) begin
-            cyccnt = 0;
-            {bitcnt,highlow} = 3'h0;
-            {spi_ssn, spi_sck, spi_mosi} = 3'b111;
-            rbyte = 8'h0;
+            cyccnt <= 0;
+            {bitcnt,highlow} <= 3'h0;
+            {spi_ssn, spi_sck, spi_mosi} <= 3'b111;
+            rbyte <= 8'h0;
         end else if(cyccnt<clkdivreg) begin
-            cyccnt++;
+            cyccnt <= cyccnt + 1;
         end else begin
-            spi_ssn  = ~chipselect;
-            spi_sck = scken ? highlow : 1'b1;
-            if(highlow) // posedge of spi_sck, capture spi_miso
-                rbyte[7-bitcnt]= spi_miso;
-            else        // negedge of spi_sck, set spi_mosi
-                spi_mosi = wbyte[7-bitcnt];
-            {bitcnt,highlow}++;
-            cyccnt = 0;
+            spi_ssn <= ~chipselect;
+            spi_sck <= scken ? highlow : 1'b1;
+            if(highlow)                                    // posedge of spi_sck, capture spi_miso
+                rbyte[7-bitcnt] <= spi_miso;
+            else                                           // negedge of spi_sck, set spi_mosi
+                spi_mosi <= wbyte[7-bitcnt];
+            {bitcnt,highlow} <= {bitcnt,highlow} + 3'h1;
+            cyccnt <= 0;
         end
     end
 
 always @ (posedge clk or negedge rstn)
     if(~rstn) begin
-        start_last   = 1'b0;
-        clkdivreg    = 0;
-        {cmdr,acmdr} = 0;
-        {cmdrsp, acmdrsp, rwrsp} = 0;
-        {cmdres, acmdres} = 0;
-        {waitc,prec,startc,cmdc,cmdrwait,cmdrc,acmdc,acmdrwait,acmdrc,midc,rwc,stopc,rec,lastc} = 0;
-        {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} = 0;
-        {chipselect,scken,wbyte} = {2'b00, 8'hff};
+        start_last   <= 1'b0;
+        clkdivreg    <= 0;
+        {cmdr,acmdr} <= '0;
+        {cmdrsp, acmdrsp, rwrsp} <= '0;
+        {cmdres, acmdres} <= '0;
+        {waitc,prec,startc,cmdc,cmdrwait,cmdrc,acmdc,acmdrwait,acmdrc,midc,rwc,stopc,rec,lastc} <= '0;
+        {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} <= '0;
+        {chipselect,scken,wbyte} <= {2'b00, 8'hff};
         {rvalid, rdata, rindex} = 0;
     end else begin
         {rvalid, rdata, rindex} = 0;
         if(~start) begin
-            start_last   = 1'b0;
-            clkdivreg    = 0;
-            {cmdr,acmdr} = 0;
-            {cmdrsp, acmdrsp, rwrsp} = 0;
-            {cmdres, acmdres} = 0;
-            {waitc,prec,startc,cmdc,cmdrwait,cmdrc,acmdc,acmdrwait,acmdrc,midc,rwc,stopc,rec,lastc} = 0;
-            {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} = 0;
-            {chipselect,scken,wbyte} = {2'b00, 8'hff};
+            start_last   <= 1'b0;
+            clkdivreg    <= 0;
+            {cmdr,acmdr} <= '0;
+            {cmdrsp, acmdrsp, rwrsp} <= '0;
+            {cmdres, acmdres} <= '0;
+            {waitc,prec,startc,cmdc,cmdrwait,cmdrc,acmdc,acmdrwait,acmdrc,midc,rwc,stopc,rec,lastc} <= '0;
+            {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} <= '0;
+            {chipselect,scken,wbyte} <= {2'b00, 8'hff};
         end else if(~start_last) begin
-            start_last   = 1'b1;
-            clkdivreg    = clkdiv<2 ? 2 : clkdiv;
-            {cmdr,acmdr} = {cmd, acmd};
-            {cmdrsp, acmdrsp, rwrsp} = 0;
-            {cmdres, acmdres} = 0;
-            {waitc    ,prec    ,startc    ,cmdc    ,cmdrwait               ,cmdrc    ,acmdc    ,acmdrwait               ,acmdrc    ,midc    ,rwc                       ,stopc    ,rec    ,lastc   } = 
-            {waitcycle,precycle,startcycle,cmdcycle,(cmdcycle>0)?8'h20:8'h0,cmdrcycle,acmdcycle,(acmdcycle>0)?8'h20:8'h0,acmdrcycle,midcycle,(midcycle>0)?16'd514:16'd0,stopcycle,recycle,8'h2    };
-            {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} = 0;
-            {chipselect,scken,wbyte} = {2'b00, 8'hff};
+            start_last   <= 1'b1;
+            clkdivreg    <= clkdiv<2 ? 2 : clkdiv;
+            {cmdr,acmdr} <= {cmd, acmd};
+            {cmdrsp, acmdrsp, rwrsp} <= '0;
+            {cmdres, acmdres} <= '0;
+            {waitc,prec,startc,cmdc,cmdrwait,cmdrc,acmdc,acmdrwait,acmdrc,midc,rwc,stopc,rec,lastc} <= {waitcycle,precycle,startcycle,cmdcycle,(cmdcycle>0)?8'h20:8'h0,cmdrcycle,acmdcycle,(acmdcycle>0)?8'h20:8'h0,acmdrcycle,midcycle,(midcycle>0)?16'd514:16'd0,stopcycle,recycle,8'h2};
+            {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} <= '0;
+            {chipselect,scken,wbyte} <= {2'b00, 8'hff};
         end else if(bytestart) begin
-            if(waitc>0) begin
-                waitc--;
-                {chipselect,scken,wbyte} = {2'b00, 8'hff};
+            if         (waitc>0) begin
+                waitc <= waitc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b00, 8'hff};
             end else if(prec>0) begin
-                prec--;
-                {chipselect,scken,wbyte} = {2'b01, 8'hff};
+                prec <= prec - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b01, 8'hff};
             end else if(startc>0) begin
-                startc--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
+                startc <= startc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
             end else if(cmdc>0) begin
-                cmdc--;
-                {chipselect,scken,wbyte} = {2'b11, cmdr[cmdc*8+:8]};
-            end else if(cmdrwait>0) begin     iscmdr = 1'b1;
-                cmdrwait--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
-            end else if(cmdrc>0) begin        iscmdres = 1'b1;
-                cmdrc--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
+                cmdc <= cmdc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, cmdr[(cmdc-8'd1)*8+:8]};
+            end else if(cmdrwait>0) begin     iscmdr <= 1'b1;
+                cmdrwait <= cmdrwait - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
+            end else if(cmdrc>0) begin        iscmdres <= 1'b1;
+                cmdrc <= cmdrc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
             end else if(acmdc>0) begin
-                acmdc--;
-                {chipselect,scken,wbyte} = {2'b11, acmdr[acmdc*8+:8]};
-            end else if(acmdrwait>0) begin    isacmdr = 1'b1;
-                acmdrwait--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
-            end else if(acmdrc>0) begin       isacmdres = 1'b1;
-                acmdrc--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
-            end else if(midc>0) begin         ismidc = 1'b1;
-                midc--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
-            end else if(rwc>0) begin          isrwc  = 1'b1;
-                rwc--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
+                acmdc <= acmdc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, acmdr[(acmdc-8'd1)*8+:8]};
+            end else if(acmdrwait>0) begin    isacmdr <= 1'b1;
+                acmdrwait <= acmdrwait - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
+            end else if(acmdrc>0) begin       isacmdres <= 1'b1;
+                acmdrc <= acmdrc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
+            end else if(midc>0) begin         ismidc <= 1'b1;
+                midc <= midc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
+            end else if(rwc>0) begin          isrwc  <= 1'b1;
+                rwc <= rwc - 16'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
             end else if(stopc>0) begin
-                stopc--;
-                {chipselect,scken,wbyte} = {2'b11, 8'hff};
+                stopc <= stopc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b11, 8'hff};
             end else if(rec>0) begin
-                rec--;
-                {chipselect,scken,wbyte} = {2'b01, 8'hff};
+                rec <= rec - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b01, 8'hff};
             end else if(lastc>0) begin
-                lastc--;
-                {chipselect,scken,wbyte} = {2'b00, 8'hff};
+                lastc <= lastc - 8'd1;
+                {chipselect,scken,wbyte} <= {2'b00, 8'hff};
             end else begin
-                {chipselect,scken,wbyte} = {2'b00, 8'hff};
+                {chipselect,scken,wbyte} <= {2'b00, 8'hff};
             end
         end else if(byteend) begin
             if(iscmdr && ~rbyte[7]) begin
-                cmdrsp    = rbyte;
-                cmdrwait  = 0;
+                cmdrsp    <= rbyte;
+                cmdrwait  = 8'd0;
             end
-            if( iscmdres) cmdres[cmdrc*8+:8] = rbyte;
+            if( iscmdres) cmdres[cmdrc*8+:8] <= rbyte;
             if(isacmdr && ~rbyte[7]) begin
-                acmdrsp   = rbyte;
-                acmdrwait = 0;
+                acmdrsp   <= rbyte;
+                acmdrwait <= 8'd0;
             end
-            if(isacmdres) acmdres[acmdrc*8+:8] = rbyte;
+            if(isacmdres) acmdres[acmdrc*8+:8] <= rbyte;
             if(ismidc && rbyte==8'hFE) begin
-                rwrsp = rbyte;
-                midc  = 0;
+                rwrsp <= rbyte;
+                midc  <= 8'd0;
             end
             if(isrwc) begin
                 {rvalid, rdata} = {1'b1, rbyte};
                 rindex = rwc;
             end
-            {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} = 0;
+            {iscmdr, isacmdr,iscmdres,isacmdres,ismidc,isrwc} <= '0;
         end
     end
-
-assign done = start && start_last && (lastc==0);
 
 endmodule
